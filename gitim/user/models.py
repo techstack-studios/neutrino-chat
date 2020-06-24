19ae553bfd031a3b97e4ebcda93918e8b1c3c486
steveyang137 <steveyang137@gmail.com>
2020-06-24 19:45:50 +0800

    给user.Users增加了__str__方法

--- gitim/user/models.py
+++ gitim/user/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from random import randint
 
 
 # Create your models here.
 
 class Users(models.Model):
-    uuid = models.CharField(max_length=10)
+    GID = models.IntegerField(default=randint(0, 1000000000))
     name = models.CharField(max_length=20)
     gender = models.CharField(max_length=6)
-    age = models.DateTimeField
-    signature = models.CharField(max_length=50)
+    signature = models.CharField(max_length=50, default='这个人并没有注意到个人简介这个功能，真遗憾')
     email = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.GID + self.name
